76c135fc5d4e886dc2444d1983f6880d03cdcf60
Antoine Salesse <antoinesalesse@gmail.com>
2023-07-12 10:13:57 +0200

    💚Fix archunit test on secondary adapters

--- example-app/infrastructure/springboot/src/architectureTest/kotlin/fr/tawane/example/app/infrastructure/adapters/secondary/InfrastructureArchitectureTest.kt
+++ example-app/infrastructure/springboot/src/architectureTest/kotlin/fr/tawane/example/app/infrastructure/adapters/secondary/InfrastructureArchitectureTest.kt
@@ -2,6 +2,7 @@ package fr.tawane.example.app.infrastructure.adapters.secondary
 
 import com.tngtech.archunit.core.domain.JavaClass
 import com.tngtech.archunit.core.importer.ClassFileImporter
+import com.tngtech.archunit.core.importer.Location
 import com.tngtech.archunit.lang.ArchCondition
 import com.tngtech.archunit.lang.ArchRule
 import com.tngtech.archunit.lang.ConditionEvents
@@ -9,33 +10,37 @@ import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
 import com.tngtech.archunit.thirdparty.com.google.common.base.Preconditions
 import org.junit.jupiter.api.Test
 import java.util.regex.Pattern
-import kotlin.reflect.KVisibility.INTERNAL
 
 const val gatewaysPackage = "fr.tawane.example.app.domain.gateways"
 const val secondaryAdaptersPackage = "fr.tawane.example.app.infrastructure.adapters.secondary"
 
 internal class InfrastructureArchitectureTest {
+  private val locationPattern = Pattern.compile(".*/build/classes/[^/]+/[^/]+[tT]est/.*")
+  private val excludeTestClasses = { location: Location -> location.matches(locationPattern).not() }
 
   @Test
   internal fun `all secondary adapters should implement gateways`() {
-    val locationPattern = Pattern.compile(".*/build/classes/[^/]+/[^/]+[tT]est/.*")
     val importedClasses = ClassFileImporter()
-      .withImportOption { location -> location.matches(locationPattern).not() }
+      .withImportOption(excludeTestClasses)
       .importPackages(secondaryAdaptersPackage)
 
     val implementAGateway = object : ArchCondition<JavaClass>("Implement a gateway") {
       override fun check(javaClass: JavaClass, events: ConditionEvents) {
         val implementsAGateway: Boolean =
-          javaClass.reflect().kotlin.visibility == INTERNAL || javaClass.interfaces.any { interfaze ->
-            interfaze.name.startsWith(
-              gatewaysPackage,
-            )
-          }
+          javaClass.isAnonymousClass || javaClass.hasAFrameworkAnnotation() || javaClass.implements(gatewaysPackage)
         Preconditions.checkState(implementsAGateway, javaClass)
       }
     }
 
     val rule: ArchRule = classes().should(implementAGateway)
     rule.check(importedClasses)
+  }
+
+  private fun JavaClass.implements(interfacePackage: String) = interfaces.any { interfaze ->
+    interfaze.name.startsWith(interfacePackage)
+  }
+
+  private fun JavaClass.hasAFrameworkAnnotation() = annotations.any { annotation ->
+    annotation.type.name.startsWith("org.springframework") || annotation.type.name.startsWith("jakarta")
   }
 }
